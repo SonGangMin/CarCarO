c05c5a2f993baf72209295239c6d152045ecc49f
SonGangMin <skm0628@naver.com>
2023-04-21 16:04:01 +0900

    게시판추가1(수정중)

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const path = require('path');
-const session = require('express-session');
-const dotenv = require('dotenv');
-const ejs = require('ejs');
-const nuncjucks = require('nunjucks');
-const passport = require('passport');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const ejs = require("ejs");
+const nuncjucks = require("nunjucks");
+const passport = require("passport");
 
 dotenv.config();
-const pageRouter = require('./routes/page');
-const authRouter = require('./routes/auth');
-const carRouter = require('./routes/car');
-const {sequelize} = require('./models');
-const passportConfig = require('./passport');
+const pageRouter = require("./routes/page");
+const authRouter = require("./routes/auth");
+const carRouter = require("./routes/car");
+const boardRouter = require("./routes/board");
+const { sequelize } = require("./models");
+const passportConfig = require("./passport");
 
 const app = express();
 passportConfig();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // nuncjucks.configure('views', {
 //   express: app,
 //   watch: true,
@@ -31,26 +32,29 @@ app.set('view engine', 'ejs');
 //     console.error(err);
 //   });
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-}));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', pageRouter);
-app.use('/auth', authRouter);
-app.use('/car', carRouter);
+app.use("/", pageRouter);
+app.use("/auth", authRouter);
+app.use("/car", carRouter);
+app.use("/boards", boardRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -60,13 +64,13 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기중');
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
 });
 
 module.exports = app;
---
+++ routes/board.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const models = require("../models");
+const multer = require("multer");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const Board = await models.boards.findAll({
+      nest: true,
+      include: [
+        {
+          attributes: ["name"],
+          model: models.users,
+          as: "user",
+        },
+      ],
+      order: [["createdAt", "DESC"]],
+    });
+    console.log("자료확인--", Board[0]);
+    res.render("board", {
+      twits: Board,
+      title: "커뮤니티",
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.get("/newpost", (req, res, next) => {
+  res.render("newpost", { title: "회원가입" });
+});
+
+// const upload = multer();
+router.post("/", async (req, res, next) => {
+  console.log("req.body =>", req.body);
+  try {
+    await models.boards.create({
+      postId: null,
+      title: req.body.title,
+      content: req.body.content,
+      createdAt: null,
+      updatedAt: null,
+      user_id: req.user.id,
+    });
+    res.redirect("/boards");
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+module.exports = router;
