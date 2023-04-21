e5de7d2e0574f74b3f0e73414b55cae04f917519
SonGangMin <skm0628@naver.com>
2023-04-22 01:12:59 +0900

    노트북

--- controllers/page.js
+++ controllers/page.js
@@ -1,32 +1,27 @@
 // 메인 페이지
 exports.renderMain = (req, res) => {
-    res.render('index', {title: 'CarcarO'});
+  res.render("index", { title: "CarcarO" });
 };
-
 
 // 로그인, 회원가입
 exports.renderJoin = (req, res) => {
-    res.render('join', {title: '회원가입'});
+  res.render("join", { title: "회원가입" });
 };
 
 exports.renderLogin = (req, res) => {
-
-
-
-    res.render('login', {title: '로그인'});
+  res.render("login", { title: "로그인" });
 };
-
 
 // 내차 찾기, 내차 팔기
 exports.renderFindcar = (req, res) => {
-    res.render('findcar', {title: '내차찾기'});
+  res.render("findcar", { title: "내차찾기" });
 };
 
 exports.renderSalecar = (req, res) => {
-    res.render('carsale', {title: '내차팔기'});
+  res.render("carsale", { title: "내차팔기" });
 };
 
 // 마이페이지
 exports.renderMypage = (req, res) => {
-    res.render('mypage', {title: '마이페이지'});
+  res.render("mypage", { title: "마이페이지" });
 };
