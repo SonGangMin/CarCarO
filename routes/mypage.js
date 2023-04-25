d4964f699352576a7c7dbbc98febfa5b005a024c
weise5329 <austria-ra@hanmail.net>
2023-04-25 09:31:31 +0900

    lsh

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { renderMypage } = require('../controllers/page');
+const mypageController = require('../controllers/mypage');
 
 const router = express.Router();
 
 router.get('/', renderMypage);
 
-router.get('/router', renderMypage);
+// 회원 정보 수정 페이지 렌더링
+router.get('/edit', mypageController.renderEditPage);
+
+// 회원 정보 수정 처리
+router.post('/edit', mypageController.updateUserInfo);
 
 module.exports = router;
