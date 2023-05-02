a8849dc3ea06444109215cff47086584c4b7bb67
SonGangMin <skm0628@naver.com>
2023-05-02 11:33:21 +0900

    차량검색수정중..

--- models/likes.js
+++ models/likes.js
@@ -7,16 +7,20 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       primaryKey: true
     },
-    user_id: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
     car_num: {
       type: DataTypes.BIGINT,
       allowNull: false,
       references: {
         model: 'cars',
         key: 'num'
+      }
+    },
+    user_id: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id'
       }
     }
   }, {
@@ -31,6 +35,13 @@ module.exports = function(sequelize, DataTypes) {
         using: "BTREE",
         fields: [
           { name: "number" },
+        ]
+      },
+      {
+        name: "likes_FK_1",
+        using: "BTREE",
+        fields: [
+          { name: "user_id" },
         ]
       },
       {
