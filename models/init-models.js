f5b36a1792a51a25be1bc48938b545ce45c6aa2a
weise5329 <austria-ra@hanmail.net>
2023-05-10 13:52:51 +0900

    차 데이터 추가

--- models/init-models.js
+++ models/init-models.js
@@ -20,22 +20,22 @@ function initModels(sequelize) {
   var likes = _likes(sequelize, DataTypes);
   var users = _users(sequelize, DataTypes);
 
-  comments.belongsTo(boards, { as: "post", foreignKey: "post_id" });
-  boards.hasMany(comments, { as: "comments", foreignKey: "post_id" });
-  hashtags.belongsTo(cars, { as: "cars_num_car", foreignKey: "cars_num" });
-  cars.hasMany(hashtags, { as: "hashtags", foreignKey: "cars_num" });
-  likes.belongsTo(cars, { as: "car_num_car", foreignKey: "car_num" });
-  cars.hasMany(likes, { as: "likes", foreignKey: "car_num" });
-  boards.belongsTo(users, { as: "user", foreignKey: "user_id" });
-  users.hasMany(boards, { as: "boards", foreignKey: "user_id" });
-  cars.belongsTo(users, { as: "user", foreignKey: "user_id" });
-  users.hasMany(cars, { foreignKey: "user_id" });
-  comments.belongsTo(users, { as: "user", foreignKey: "user_id" });
-  users.hasMany(comments, { as: "comments", foreignKey: "user_id" });
-  inquirys.belongsTo(users, { as: "user", foreignKey: "user_id" });
-  users.hasMany(inquirys, { as: "inquiries", foreignKey: "user_id" });
-  likes.belongsTo(users, { as: "user", foreignKey: "user_id" });
-  users.hasMany(likes, { as: "likes", foreignKey: "user_id" });
+  comments.belongsTo(boards, { as: "post", foreignKey: "post_id"});
+  boards.hasMany(comments, { as: "comments", foreignKey: "post_id"});
+  hashtags.belongsTo(cars, { as: "cars_num_car", foreignKey: "cars_num"});
+  cars.hasMany(hashtags, { as: "hashtags", foreignKey: "cars_num"});
+  likes.belongsTo(cars, { as: "car_num_car", foreignKey: "car_num"});
+  cars.hasMany(likes, { as: "likes", foreignKey: "car_num"});
+  boards.belongsTo(users, { as: "user", foreignKey: "user_id"});
+  users.hasMany(boards, { as: "boards", foreignKey: "user_id"});
+  cars.belongsTo(users, { as: "user", foreignKey: "user_id"});
+  users.hasMany(cars, { as: "cars", foreignKey: "user_id"});
+  comments.belongsTo(users, { as: "user", foreignKey: "user_id"});
+  users.hasMany(comments, { as: "comments", foreignKey: "user_id"});
+  inquirys.belongsTo(users, { as: "user", foreignKey: "user_id"});
+  users.hasMany(inquirys, { as: "inquiries", foreignKey: "user_id"});
+  likes.belongsTo(users, { as: "user", foreignKey: "user_id"});
+  users.hasMany(likes, { as: "likes", foreignKey: "user_id"});
 
   return {
     advertises,
